3da5776ac283d5d8f4b6babcb27f0add4c9059c0
Isabel Henrique <6isinha6@gmail.com>
2024-02-10 18:20:34 -0300

    main view

--- src/main/java/com/dev/javafxjdbc/Main.java
+++ src/main/java/com/dev/javafxjdbc/Main.java
@@ -2,18 +2,21 @@ package com.dev.javafxjdbc;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+        Parent parent = loader.load();
+
+        Scene mainScene = new Scene(parent);
+        stage.setScene(mainScene);
+        stage.setTitle("Sample JavaFX application");
         stage.show();
     }
 
