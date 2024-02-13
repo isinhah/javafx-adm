969d0747c329a8133f19f9be0ec3de8bfa32d4e0
Isabel Henrique <6isinha6@gmail.com>
2024-02-13 18:54:43 -0300

    função de salvar novo departamento

--- src/main/java/com/dev/javafxjdbc/controllers/DepartmentListController.java
+++ src/main/java/com/dev/javafxjdbc/controllers/DepartmentListController.java
@@ -85,6 +85,7 @@ public class DepartmentListController implements Initializable {
 
             DepartmentFormController controller = loader.getController();
             controller.setDepartment(obj);
+            controller.setDepartmentService(new DepartmentService());
             controller.updateFormData();
 
             //Carrega a janela do formulário
